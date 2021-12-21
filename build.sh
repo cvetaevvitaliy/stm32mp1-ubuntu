5164efea1763522859876d110e74c8bea9e19d67
vNimych <vitaliy.nimych@gmail.com>
2021-12-21 10:52:55 +0200

    update build script

--- build.sh
+++ build.sh
@@ -4,14 +4,22 @@ export LC_ALL=C
 
 # TODO: need to make config file for configure build versions
 UBOOT_VERSION="v2021.10"
-KERNEL_VERSION="5.8.5-armv7-lpae-x11"
-ROOTFS_NAME="ubuntu-18.04.6-minimal-armhf-2021-11-02.tar.xz"
-ROOT_FS_LINK="wget -c https://rcn-ee.com/rootfs/eewiki/minfs/${ROOTFS_NAME}"
+KERNEL_VERSION="5.15.6-armv7-lpae-x11"
+ROOTFS_NAME="custom_image"
+ROOT_FS_LINK="https://rcn-ee.com/rootfs/eewiki/minfs/${ROOTFS_NAME}"
 UBUNTU_VERSION="ubuntu"
+
+UBUNTU_18="ubuntu-18.04.6-minimal-armhf-2021-11-02.tar.xz"
+UBUNTU_20="ubuntu-20.04.3-minimal-armhf-2021-11-02.tar.xz"
+DEBIAN_10="debian-10.11-minimal-armhf-2021-11-02.tar.xz"
+DEBIAN_11="debian-11.1-minimal-armhf-2021-11-02.tar.xz"
+
+DOWNLOAD_LINK="https://rcn-ee.com/rootfs/eewiki/minfs/"
 
 CUR_PATH=$(pwd)
 
 IMAGE_FILENAME=sdcard-stm32mp157.img
+MOUNT_PATH="./output/rootfs"
 
 
 function usage()
@@ -29,6 +37,7 @@ function usage()
     echo "cleanall           -clean uboot, kernel, rootfs"
     echo "check              -check the environment of building"
     echo "info               -see the current  building information"
+    echo "qemu               -Mount image and usage QEMU Full-system emulation"
     echo ""
 }
 
@@ -185,7 +194,6 @@ function check(){
 
 function calenup(){
     echo ""
-
     echo "Try umount folder: '${MOUNT_PATH}'"
     sudo umount ${MOUNT_PATH}
     case "$?" in 
@@ -200,47 +208,45 @@ function calenup(){
     	*) echo "Error clean '${LOOP_DEVICE}' device " ;;
     esac
     echo "Finish"
-    exit 0
+    #exit 0
 }
 
 
+function init_image(){
 
-function create_image(){
-    echo "============================================"
-    echo "Start create image"
-    echo ""
-    check_output_dir
-    
     if ! [ -f ./output/${IMAGE_FILENAME} ]; then
-    	echo "Create ${IMAGE_FILENAME}"
-    	dd if=/dev/zero of=./output/${IMAGE_FILENAME} bs=4096M count=2
-    	check
+        echo "Create ${IMAGE_FILENAME}"
+        dd if=/dev/zero of=./output/${IMAGE_FILENAME} bs=4096M count=2
+        check
     
-    	echo "Create file systems ${IMAGE_FILENAME}"
-    	sgdisk --resize-table=128 -a 1 \
-        	-n 1:34:545    -c 1:fsbl1   \
-        	-n 2:546:1057  -c 2:fsbl2   \
-        	-n 3:1058:5153 -c 3:ssbl    \
-        	-n 4:5154:     -c 4:rootfs  \
-        	-p ./output/${IMAGE_FILENAME}
-        check	
+        echo "Create file systems ${IMAGE_FILENAME}"
+        sgdisk --resize-table=128 -a 1 \
+            -n 1:34:545    -c 1:fsbl1   \
+            -n 2:546:1057  -c 2:fsbl2   \
+            -n 3:1058:5153 -c 3:ssbl    \
+            -n 4:5154:     -c 4:rootfs  \
+            -p ./output/${IMAGE_FILENAME}
+        check
     
-    	echo ""
-    	echo "Set legacy BIOS partition:"
-   		sgdisk -A 4:set:2 ./output/${IMAGE_FILENAME}
-   		check
-    else 
-    	echo "Found image ${IMAGE_FILENAME}"
-    	echo ""    
+        echo ""
+        echo "Set legacy BIOS partition:"
+           sgdisk -A 4:set:2 ./output/${IMAGE_FILENAME}
+           check
+    else
+        echo "Found image ${IMAGE_FILENAME}"
+        echo ""
     fi
     
-    
     if ! [ -f ./output/${IMAGE_FILENAME} ]; then
-    	echo "Error create ${IMAGE_FILENAME}"  
-    	echo ""
-    	exit;
+        echo "Error create ${IMAGE_FILENAME}"
+        echo ""
+        exit;
     fi
-    
+
+}
+
+function create_loop(){
+
     echo "Create loop device, need root access for use losetup"
     LOOP_DEVICE=$(sudo losetup --partscan --show --find ./output/${IMAGE_FILENAME})
     echo "Create loop device '${LOOP_DEVICE}'"
@@ -248,27 +254,32 @@ function create_image(){
     echo "Print info about loop"
     ls -l ${LOOP_DEVICE}*
     echo ""
-    
-    
-    echo "============================================"
-    echo "Install U-Boot bootloader version ${UBOOT_VERSION}:"
-    if [ -f ./output/u-boot.img ]; then
-    	sudo dd if=./output/u-boot-spl.stm32 of=${LOOP_DEVICE}0p1
-    	sudo dd if=./output/u-boot-spl.stm32 of=${LOOP_DEVICE}p2
-		sudo dd if=./output/u-boot.img of=${LOOP_DEVICE}p3
-    else 
-    	echo "First need build U-Boot, please make './build.sh uboot'"
-	exit 0
-    fi
-    
-    echo ""
-    
+
+}
+
+function format_rootfs(){
+
     echo "Format RootFS Partition:"
     echo ""
     sudo mkfs.ext4 -L rootfs ${LOOP_DEVICE}p4
     check
+}
 
-    MOUNT_PATH="./output/rootfs"
+function install_uboot(){
+    echo "============================================"
+    echo "Install U-Boot bootloader version ${UBOOT_VERSION}:"
+    if [ -f ./output/u-boot.img ]; then
+        sudo dd if=./output/u-boot-spl.stm32 of=${LOOP_DEVICE}0p1
+        sudo dd if=./output/u-boot-spl.stm32 of=${LOOP_DEVICE}p2
+        sudo dd if=./output/u-boot.img of=${LOOP_DEVICE}p3
+    else
+        echo "First need build U-Boot, please make './build.sh uboot'"
+    exit 0
+    fi
+
+}
+
+function copy_kernel(){
 
     echo "Mount rootfs file system to ${MOUNT_PATH}"
     echo ""
@@ -278,12 +289,112 @@ function create_image(){
 
     echo ""
     if [ -d ./armv7-lpae-multiplatform/deploy ]; then
-	kernel_ver=$(basename ./armv7-lpae-multiplatform/deploy/*.zImage | rev | cut -c 8- | rev )
-	echo "Kernel version ${kernel_ver}"
+    kernel_ver=$(basename ./armv7-lpae-multiplatform/deploy/*.zImage | rev | cut -c 8- | rev )
+    echo "Kernel version ${kernel_ver}"
     else
-	echo "First need build Kernel, please make './build.sh kernel'"
-	exit 0
+    echo "First need build Kernel, please make './build.sh kernel'"
+    exit 0
     fi
+
+}
+
+
+function copy_all_configs_and_modules(){
+    echo ""
+    echo "Setup extlinux.conf"
+    sudo mkdir -p ${MOUNT_PATH}/boot/extlinux/
+    sudo sh -c "echo 'label Linux ${kernel_ver}' > ${MOUNT_PATH}/boot/extlinux/extlinux.conf"
+    sudo sh -c "echo '    kernel /boot/vmlinuz-${kernel_ver}' >> ${MOUNT_PATH}/boot/extlinux/extlinux.conf"
+    sudo sh -c "echo '    append console=ttySTM0,115200 console=tty1,115200 fbcon=rotate:3 root=/dev/mmcblk0p4 ro rootfstype=ext4 rootwait' >> ${MOUNT_PATH}/boot/extlinux/extlinux.conf"
+    sudo sh -c "echo '    fdtdir /boot/dtbs/${kernel_ver}/' >> ${MOUNT_PATH}/boot/extlinux/extlinux.conf"
+    check
+
+    echo ""
+    echo "Copy Kernel Image"
+    sudo cp -v ./armv7-lpae-multiplatform/deploy/${kernel_ver}.zImage ${MOUNT_PATH}/boot/vmlinuz-${kernel_ver}
+    check
+
+
+    echo ""
+    echo "Copy Kernel Device Tree Binaries"
+    sudo mkdir -p ${MOUNT_PATH}/boot/dtbs/${kernel_ver}/
+    sudo tar xf ./armv7-lpae-multiplatform/deploy/${kernel_ver}-dtbs.tar.gz -C ${MOUNT_PATH}/boot/dtbs/${kernel_ver}/
+    check
+
+    echo ""
+    echo "Copy Kernel Modules"
+    sudo tar xfv ./armv7-lpae-multiplatform/deploy/${kernel_ver}-modules.tar.gz -C ${MOUNT_PATH}
+    check
+
+    sudo sh -c "echo 'auto eth0' >> ${MOUNT_PATH}/etc/network/interfaces"
+    sudo sh -c "echo 'iface eth0 inet dhcp' >> ${MOUNT_PATH}/etc/network/interfaces"
+
+    echo ""
+    echo "Copy WiFi firmware"
+    sudo mkdir -p ${MOUNT_PATH}/lib/firmware/brcm/
+    sudo cp -v ./wifi_firmware/brcmfmac43430* ${MOUNT_PATH}/lib/firmware/brcm/
+    sudo cp -v ./wifi_firmware/brcmfmac43430-sdio.txt ${MOUNT_PATH}/lib/firmware/brcm/brcmfmac43430-sdio.st,stm32mp157c-dk2.txt
+    check
+
+# activate welcome message
+    sudo chmod +x ${MOUNT_PATH}/etc/update-motd.d/00-header
+    sudo chmod +x ${MOUNT_PATH}/etc/update-motd.d/10-help-text
+    sudo chmod +x ${MOUNT_PATH}/etc/update-motd.d/50-motd-news
+    sudo chmod +x ${MOUNT_PATH}/etc/update-motd.d/80-esm
+    sudo chmod +x ${MOUNT_PATH}/etc/update-motd.d/80-livepatch
+
+    echo ""
+    echo "Copy helper scripts"
+    sudo cp -v ./script/resize_sd.sh ${MOUNT_PATH}/usr/bin/
+    sudo cp -v ./script/activate_wifi.sh ${MOUNT_PATH}/usr/bin/
+    
+    echo ""
+    echo "File Systems Table (/etc/fstab)"
+    sudo sh -c "echo '/dev/mmcblk0p4  /  auto  errors=remount-ro  0  1' >> ${MOUNT_PATH}/etc/fstab"
+    check
+    
+    sync
+    case "$?" in
+    0) echo "Sync OK"  ;;
+    *) echo "Error sync " ;;
+    esac
+}
+
+
+function create_image(){
+    echo "============================================"
+    echo "Start create image"
+    echo ""
+    
+    check_output_dir
+    init_image
+    create_loop
+    install_uboot
+    format_rootfs
+    copy_kernel
+    
+    
+#    echo "Format RootFS Partition:"
+#    echo ""
+#    sudo mkfs.ext4 -L rootfs ${LOOP_DEVICE}p4
+#    check
+
+#    MOUNT_PATH="./output/rootfs"
+#
+#    echo "Mount rootfs file system to ${MOUNT_PATH}"
+#    echo ""
+#    sudo mkdir -p ${MOUNT_PATH}
+#    sudo mount /dev/loop0p4 ${MOUNT_PATH}
+#    check
+#
+#    echo ""
+#    if [ -d ./armv7-lpae-multiplatform/deploy ]; then
+#	kernel_ver=$(basename ./armv7-lpae-multiplatform/deploy/*.zImage | rev | cut -c 8- | rev )
+#	echo "Kernel version ${kernel_ver}"
+#    else
+#	echo "First need build Kernel, please make './build.sh kernel'"
+#	exit 0
+#    fi
 
 
     echo ""
@@ -324,8 +435,8 @@ function create_image(){
 	sudo tar xfv ./armv7-lpae-multiplatform/deploy/${kernel_ver}-modules.tar.gz -C ${MOUNT_PATH}
 	check
 
-	#sudo sh -c "echo 'auto eth0' >> ${MOUNT_PATH}/etc/network/interfaces"
-	#sudo sh -c "echo 'iface eth0 inet dhcp' >> ${MOUNT_PATH}/etc/network/interfaces"
+	sudo sh -c "echo 'auto eth0' >> ${MOUNT_PATH}/etc/network/interfaces"
+	sudo sh -c "echo 'iface eth0 inet dhcp' >> ${MOUNT_PATH}/etc/network/interfaces"
 
     echo ""
     echo "Copy WiFi firmware"
@@ -350,7 +461,7 @@ function create_image(){
     echo "File Systems Table (/etc/fstab)"
 	sudo sh -c "echo '/dev/mmcblk0p4  /  auto  errors=remount-ro  0  1' >> ${MOUNT_PATH}/etc/fstab"
 	check
-
+    
 	sync
 	case "$?" in 
 	0) echo "Sync OK"  ;;
@@ -361,6 +472,39 @@ function create_image(){
     
 }
 
+
+function open_qemu(){
+    echo ""
+    echo "Open QENU emulator"
+
+    echo "Create loop device, need root access for use losetup"
+    LOOP_DEVICE=$(sudo losetup --partscan --show --find ./output/${IMAGE_FILENAME})
+    echo "Create loop device '${LOOP_DEVICE}'"
+    
+    echo "Print info about loop"
+    ls -l ${LOOP_DEVICE}*
+    echo ""
+
+    echo "Mount rootfs file system to ${MOUNT_PATH}"
+    echo ""
+    sudo mkdir -p ${MOUNT_PATH}
+    sudo mount /dev/loop0p4 ${MOUNT_PATH}
+    check
+    
+    
+    sudo cp /usr/bin/qemu-arm-static  ${MOUNT_PATH}/usr/bin/
+    #sudo rm ${MOUNT_PATH}/etc/resolv.conf
+    #sudo touch ${MOUNT_PATH}/etc/resolv.conf
+    #sudo sh -c echo "nameserver 8.8.8.8" > ${MOUNT_PATH}/etc/resolv.conf
+    (exec ./ch-mount.sh -m ${MOUNT_PATH} )
+    echo "Clean up"
+    (exec ./ch-mount.sh -u ${MOUNT_PATH} )
+
+    sync
+    calenup
+
+
+}
 
 function build_cleanall(){
     echo "============================================"
@@ -379,12 +523,204 @@ function build_info(){
     echo "Build information"
 }
 
-function compress_img(){
+function mk_ubuntu_18(){
+    echo "============================================"
+    echo "Create Ubuntu 18 image"
+    
+    if ! [ -f ./download_dir/${UBUNTU_18} ]; then
+        wget -P ./download_dir ${DOWNLOAD_LINK}${UBUNTU_18}
+    fi
+    
+    UBUNTU_18_VERSION="ubuntu_18_rootfs"
+    if ! [ -d ./${UBUNTU_18_VERSION} ]; then
+        mkdir ./${UBUNTU_18_VERSION}
+        echo "Extract to ${CUR_PATH}/${UBUNTU_18_VERSION} "
+        tar xvf ./download_dir/${UBUNTU_18} -C ./${UBUNTU_18_VERSION}
+    fi
+    
+    check_output_dir
+    init_image
+    create_loop
+    install_uboot
+    format_rootfs
+    copy_kernel
+    
+    echo ""
+    echo "Copy Root File System"
+    if [ -d ./${UBUNTU_18_VERSION} ]; then
+        echo "Extract to ${CUR_PATH}/${UBUNTU_18_VERSION} "
+        sudo tar xvfp ./${UBUNTU_18_VERSION}/*/*.tar -C ${MOUNT_PATH}
+        check
+    else
+        echo "First need get rootfs, please make './build.sh ubuntu'"
+        exit 0
+    fi
+    
+    copy_all_configs_and_modules
+    
+    
+    calenup
+    
     echo "Compress SD card image"
-    TAR_NAME=$(basename ${ROOTFS_NAME} | cut -c 1-14)
+    TAR_NAME=$(basename ${UBUNTU_18} | cut -c 1-14)
     echo "${TAR_NAME}"
     # tar cfv - ./output/sdcard-stm32mp157.img | pv | xz -z -T0 - > ./output/${TAR_NAME}-full-sd-image.tar.xz
-    pv ./output/sdcard-stm32mp157.img | gzip  > ./output/${TAR_NAME}-full-sd-image.img.gz
+    pv ./output/sdcard-stm32mp157.img | gzip  > ./output/${TAR_NAME}-kernel-${KERNEL_VERSION}-full-sd-image.img.gz
+    
+}
+
+function mk_ubuntu_20(){
+    echo "============================================"
+    echo "Create Ubuntu 20 image"
+    
+    if ! [ -f ./download_dir/${UBUNTU_20} ]; then
+        echo "Download file"
+        wget -P ./download_dir ${DOWNLOAD_LINK}${UBUNTU_20}
+    fi
+    
+    UBUNTU_20_VERSION="ubuntu_20_rootfs"
+    if ! [ -d ./${UBUNTU_20_VERSION} ]; then
+        mkdir ./${UBUNTU_20_VERSION}
+        echo "Extract to ${CUR_PATH}/${UBUNTU_20_VERSION} "
+        tar xvf ./download_dir/${UBUNTU_20} -C ./${UBUNTU_20_VERSION}
+    fi
+    
+    check_output_dir
+    init_image
+    create_loop
+    install_uboot
+    format_rootfs
+    copy_kernel
+    
+    echo ""
+    echo "Copy Root File System"
+    if [ -d ./${UBUNTU_18_VERSION} ]; then
+        echo "Extract to ${CUR_PATH}/${UBUNTU_20_VERSION} "
+        sudo tar xvfp ./${UBUNTU_20_VERSION}/*/*.tar -C ${MOUNT_PATH}
+        check
+    else
+        echo "First need get rootfs, please make './build.sh ubuntu'"
+        exit 0
+    fi
+    
+    copy_all_configs_and_modules
+    
+    
+    calenup
+    
+    echo "Compress SD card image"
+    TAR_NAME=$(basename ${UBUNTU_20} | cut -c 1-14)
+    echo "${TAR_NAME}"
+    # tar cfv - ./output/sdcard-stm32mp157.img | pv | xz -z -T0 - > ./output/${TAR_NAME}-full-sd-image.tar.xz
+    pv ./output/sdcard-stm32mp157.img | gzip  > ./output/${TAR_NAME}-kernel-${KERNEL_VERSION}-full-sd-image.img.gz
+    
+    
+}
+
+
+function debian_11(){
+
+    echo "============================================"
+    echo "Create Debian 11 image"
+    
+    if ! [ -f ./download_dir/${DEBIAN_11} ]; then
+        echo "Download file"
+        wget -P ./download_dir ${DOWNLOAD_LINK}${DEBIAN_11}
+    fi
+    
+    DEBIAN_VERSION="debian_11_rootfs"
+    if ! [ -d ./${DEBIAN_VERSION} ]; then
+        mkdir ./${DEBIAN_VERSION}
+        echo "Extract to ${CUR_PATH}/${DEBIAN_VERSION} "
+        tar xvf ./download_dir/${DEBIAN_11} -C ./${DEBIAN_VERSION}
+    fi
+    
+    check_output_dir
+    init_image
+    create_loop
+    install_uboot
+    format_rootfs
+    copy_kernel
+    
+    echo ""
+    echo "Copy Root File System"
+    if [ -d ./${DEBIAN_VERSION} ]; then
+        echo "Extract to ${CUR_PATH}/${DEBIAN_VERSION} "
+        sudo tar xvfp ./${DEBIAN_VERSION}/*/*.tar -C ${MOUNT_PATH}
+        check
+    else
+        echo "First need get rootfs, please make './build.sh ubuntu'"
+        exit 0
+    fi
+    
+    copy_all_configs_and_modules
+    
+    
+    calenup
+    
+    echo "Compress SD card image"
+    TAR_NAME=$(basename ${DEBIAN_11} | cut -c 1-11)
+    echo "${TAR_NAME}"
+    # tar cfv - ./output/sdcard-stm32mp157.img | pv | xz -z -T0 - > ./output/${TAR_NAME}-full-sd-image.tar.xz
+    pv ./output/sdcard-stm32mp157.img | gzip  > ./output/${TAR_NAME}-kernel-${KERNEL_VERSION}-full-sd-image.img.gz
+
+}
+
+function debian_10(){
+
+    echo "============================================"
+    echo "Create Debian 10 image"
+    
+    if ! [ -f ./download_dir/${DEBIAN_10} ]; then
+        echo "Download file"
+        wget -P ./download_dir ${DOWNLOAD_LINK}${DEBIAN_10}
+    fi
+    
+    DEBIAN_VERSION_10="debian_10_rootfs"
+    if ! [ -d ./${DEBIAN_VERSION_10} ]; then
+        mkdir ./${DEBIAN_VERSION_10}
+        echo "Extract to ${CUR_PATH}/${DEBIAN_VERSION_10} "
+        tar xvf ./download_dir/${DEBIAN_10} -C ./${DEBIAN_VERSION_10}
+    fi
+    
+    check_output_dir
+    init_image
+    create_loop
+    install_uboot
+    format_rootfs
+    copy_kernel
+    
+    echo ""
+    echo "Copy Root File System"
+    if [ -d ./${DEBIAN_10_VERSION} ]; then
+        echo "Extract to ${CUR_PATH}/${DEBIAN_VERSION_10} "
+        sudo tar xvfp ./${DEBIAN_VERSION_10}/*/*.tar -C ${MOUNT_PATH}
+        check
+    else
+        echo "First need get rootfs, please make './build.sh ubuntu'"
+        exit 0
+    fi
+    
+    copy_all_configs_and_modules
+    
+    
+    calenup
+    
+    echo "Compress SD card image"
+    TAR_NAME=$(basename ${DEBIAN_10} | cut -c 1-11)
+    echo "${TAR_NAME}"
+    # tar cfv - ./output/sdcard-stm32mp157.img | pv | xz -z -T0 - > ./output/${TAR_NAME}-full-sd-image.tar.xz
+    pv ./output/sdcard-stm32mp157.img | gzip  > ./output/${TAR_NAME}-kernel-${KERNEL_VERSION}-full-sd-image.img.gz
+
+}
+
+
+function compress_img(){
+    echo "Compress SD card image"
+    TAR_NAME=${ROOTFS_NAME}
+    echo "${TAR_NAME}"
+    # tar cfv - ./output/sdcard-stm32mp157.img | pv | xz -z -T0 - > ./output/${TAR_NAME}-full-sd-image.tar.xz
+    pv ./output/sdcard-stm32mp157.img | gzip  > ./output/${TAR_NAME}-${KERNEL_VERSION}-full-sd-image.img.gz
 }
 
 
@@ -406,19 +742,24 @@ OPTIONS="${@:-allff}"
 for option in ${OPTIONS}; do
     # echo "processing option: $option"
     case $option in
-	all) build_all ;;
-	uboot) build_uboot ;;
-	kernel) build_kernel ;;
-	modules) build_modules ;;
-	toolchain) get_toolchain ;;
-	debian) download_debian ;;
-	ubuntu) download_ubuntu ;;
-	mkimage) create_image ;;
-	cleanall) build_cleanall ;;
-	check) check_env ;;
-	compress_img) compress_img ;;
-	info) build_info ;;
-	*) usage ;;
+    all) build_all ;;
+    uboot) build_uboot ;;
+    kernel) build_kernel ;;
+    modules) build_modules ;;
+    toolchain) get_toolchain ;;
+    debian) download_debian ;;
+    ubuntu) download_ubuntu ;;
+    mkimage) create_image ;;
+    qemu) open_qemu ;;
+    cleanall) build_cleanall ;;
+    check) check_env ;;
+    compress_img) compress_img ;;
+    info) build_info ;;
+    mkubuntu18) mk_ubuntu_18 ;;
+    mkubuntu20) mk_ubuntu_20 ;;
+    mkdebian11) debian_11 ;;
+    mkdebian10) debian_10 ;;
+    *) usage ;;
     esac
 done
 
